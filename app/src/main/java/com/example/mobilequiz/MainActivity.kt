ec77a3ae85f5a97acab611f8e70a675fdb9fa4e8
MatheusLimaGomes <matheus.lima.gomes@outlook.com>
2020-07-06 22:05:02 -0300

    ensure main activity xml layout, add quizActivity and set Build version

--- app/src/main/java/com/example/mobilequiz/MainActivity.kt
+++ app/src/main/java/com/example/mobilequiz/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.mobilequiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    private val viewHolder = ViewHolder.create()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupViews()
+    }
+    fun setupViews() {
+        viewHolder.buttonQuiz = findViewById<Button>(R.id.startQuizButton)
+    }
+}
+class ViewHolder {
+    var buttonQuiz: Button? = null
+
+    companion object Instance {
+        fun create(): ViewHolder = ViewHolder()
     }
 }
