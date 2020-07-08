cb387d9448e2ecb2286d71585da8cd40624aa624
MatheusLimaGomes <matheus.lima.gomes@outlook.com>
2020-07-08 11:39:17 -0300

    set colors to own files

--- app/src/main/java/com/example/mobilequiz/MainActivity.kt
+++ app/src/main/java/com/example/mobilequiz/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.mobilequiz
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.core.content.ContextCompat.startActivity
+import kotlinx.android.synthetic.main.activity_quiz.*
 
 class MainActivity : AppCompatActivity() {
     private val viewHolder = ViewHolder.create()
@@ -14,7 +17,15 @@ class MainActivity : AppCompatActivity() {
     fun setupViews() {
         viewHolder.buttonQuiz = findViewById<Button>(R.id.startQuizButton)
     }
+    fun goToQuizActivity() {
+        viewHolder.buttonQuiz.setOnClickListener(this)
+    }
 }
+
+private fun Button?.setOnClickListener(mainActivity: MainActivity) {
+
+}
+
 class ViewHolder {
     var buttonQuiz: Button? = null
 
